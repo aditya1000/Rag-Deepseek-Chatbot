a12f098f4cffaab54b4720d9b36fe419827ecd42
Aditya Nagori <aditya.mits13@gmail.com>
2025-01-29 22:57:18 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -26,7 +26,7 @@ Settings.embed_model = HuggingFaceEmbedding(
 
 #my_llm = OllamaLLM(model_name="llama2")
 
-my_llm = Ollama(model="llama3.2")  # or your preferred model
+my_llm = Ollama(model="deepseek-r1:7b")  # or your preferred model
 # ---------------------------------------
 # Streamlit App
 # ---------------------------------------
